b54a81ac7cad24f16d96f977479dcc2dd5738c27
LenaPancher <lenapancher@gmail.com>
2023-10-23 14:19:34 +0200

    move navigator in file

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import HelloText from "./src/component/HelloText";
-
-const Stack = createNativeStackNavigator();
+import Navigator from "./src/component/Navigator";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home">
-          {() => <HelloText text={"coucou"} color={"#e30404"}/>}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigator/>
   );
 };
 
