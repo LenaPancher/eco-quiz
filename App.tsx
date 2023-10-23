8b310e08caea080169f8abe17c824f1e54533b95
barbara michaud <barbara.michaud@outlook.fr>
2023-10-23 10:41:41 +0200

    adding tailwind

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import {StatusBar} from "expo-status-bar";
-import {StyleSheet, View} from "react-native";
+import {View} from "react-native";
 import HelloText from "./src/component/HelloText";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View
+      className="flex-1 items-center justify-center bg-white"
+    >
       <HelloText text={"Hello world bitches !"} color={"#f50909"}></HelloText>
       <StatusBar style="auto"/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
