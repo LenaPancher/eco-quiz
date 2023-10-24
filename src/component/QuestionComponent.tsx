7e45cc4e01001667729af41290ac7f197fc8a494
LenaPancher <lenapancher@gmail.com>
2023-10-24 12:32:23 +0200

    add style + split QuestionComponent

--- src/component/QuestionComponent.tsx
+++ src/component/QuestionComponent.tsx
@@ -1,14 +1,20 @@
-import {Button, Text, TouchableOpacity, View} from "react-native";
+import {Text, View} from "react-native";
 import {useState} from "react";
 import {QUESTIONS} from "../utils/questions";
+import ButtonComponent from "./ButtonComponent";
+import AnswerComponent from "./AnswerComponent";
+import {useNavigation} from "@react-navigation/native";
 
 interface QuestionComponent {
   level: number;
 }
 
 const QuestionComponent = ({level}: QuestionComponent) => {
+  const navigation = useNavigation();
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [score, setScore] = useState(0);
 
   const currentLevel = QUESTIONS[level - 1];
   const currentQuestion = currentLevel.questions[currentQuestionIndex];
@@ -18,31 +24,29 @@ const QuestionComponent = ({level}: QuestionComponent) => {
   };
 
   const handleNextQuestion = () => {
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setSelectedOption(null);
+
+    if (selectedOption === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestionIndex < currentLevel.questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      console.log(score);
+      navigation.navigate("Home");
+    }
   };
 
   return (
-    <View>
-      <Text>{currentQuestion.question}</Text>
-      {currentQuestion.options.map((option, index) => (
-        <TouchableOpacity
-          key={index}
-          onPress={() => handleAnswer(option)}
-          style={{
-            backgroundColor: selectedOption === option ? "green" : "white",
-            padding: 10,
-            margin: 5,
-            borderWidth: 1,
-            borderColor: "black"
-          }}
-        >
-          <Text>{option}</Text>
-        </TouchableOpacity>
-      ))}
-      <Button
-        title="Valider"
-        onPress={handleNextQuestion}
-      />
+    <View className={"bg-[#FFFFFF] h-full"}>
+      <View className={"mx-4 flex-1"}>
+        <Text className={"my-7 text-2xl"}>{currentQuestion.question}</Text>
+        <View className={"flex-1 justify-around"}>
+          <AnswerComponent questionItem={currentQuestion} onPress={handleAnswer} selectedOption={selectedOption}/>
+          <ButtonComponent onPress={handleNextQuestion} title={"Valider"} selectedOption={selectedOption}/>
+        </View>
+      </View>
     </View>
   );
 };
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -19,7 +19,7 @@ export const QUESTIONS: Level[] = [
   {
     level: 1,
     questions: [
-      new QuestionItem("Quel déchet peut être recyclé ?", ["Plastique", "Pain", "Carton", "Le verre"], "Le verre"),
+      new QuestionItem("Quel déchet peut être recyclé ?", ["Plastique", "Pain", "Carton", "Verre"], "Verre"),
       new QuestionItem("Où doit-on jeter les déchets électroniques ?", ["Poubelle", "Compost", "Évier", "Point de collecte spécialisé"], "Point de collecte spécialisé"),
       new QuestionItem("Quel matériau est biodégradable ?", ["Métal", "Plastique", "Verre", "Papier"], "Papier"),
       new QuestionItem("Comment réduire la consommation d'eau ?", ["Utiliser de l'huile", "Fermer le robinet", "Laisser couler", "Manger des légumes"], "Fermer le robinet")
@@ -28,7 +28,7 @@ export const QUESTIONS: Level[] = [
   {
     level: 2,
     questions: [
-      new QuestionItem("Quel type de transport est le plus écologique ?", ["Le vélo", "Voiture électrique", "Avion", "Moto"], "Le vélo"),
+      new QuestionItem("Quel type de transport est le plus écologique ?", ["Vélo", "Voiture électrique", "Avion", "Moto"], "Vélo"),
       new QuestionItem("Quel déchet ne va jamais dans la poubelle verte ?", ["Papier", "Verre", "Piles", "Légumes"], "Piles"),
       new QuestionItem("Que signifie 'RE-RE-RE' dans le recyclage ?", ["Réduire, Réutiliser, Recycler", "Refuser, Rire, Ranger"], "Réduire, Réutiliser, Recycler"),
       new QuestionItem("Quel est un aliment non périssable ?", ["Lait", "Poisson", "Pain", "Riz"], "Riz")
