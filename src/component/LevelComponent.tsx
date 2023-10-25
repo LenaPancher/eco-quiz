9e0c4a0ca62764e82014d9b6c37644b6fa70071e
barbara michaud <barbara.michaud@outlook.fr>
2023-10-25 15:21:23 +0200

    fix bug ts image in levelcomponent

--- src/component/LevelComponent.tsx
+++ src/component/LevelComponent.tsx
@@ -1,8 +1,8 @@
-import {Image, Pressable, View} from "react-native";
+import {Image, Pressable, View, ImageSourcePropType} from "react-native";
 
 export type levelComponentProps = {
   onPress: () => void;
-  img: string;
+  img: ImageSourcePropType;
 };
 
 const LevelComponent = ({onPress, img}: levelComponentProps) => {
@@ -16,7 +16,7 @@ const LevelComponent = ({onPress, img}: levelComponentProps) => {
           className="h-[120px] w-[120px] bg-[#419677] rounded-full justify-center items-center"
           style={{borderColor: "white", borderWidth: 6, borderStyle: "solid"}}
         >
-          <Image source={img} className="h-full w-full rounded-full"/>
+          <Image source={img} className="h-full w-full rounded-full" />
         </View>
       </Pressable>
     </View>
